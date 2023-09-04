fa24acbdf37b1a4262c0f768eecdb9658ef24f58
anhuar-ma <A01737767@tec.mx>
2023-09-03 22:50:21 -0600

    creation of .dotfiles

---
+++ .config/qtile/autostart_once.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Apply wallpaper using wal
+wal -b 282738 -i ~/Wallpaper/Aesthetic2.png &&
+
+# Start picom
+picom --config ~/.config/picom/picom.conf &
+xinput set-prop 13 347 1 &
+xinput set-prop 13 329 0.4 & 
+setxkbmap -layout us -variant colemak_dh -option caps:capslock &
+xrandr --output eDP-1 --mode 1920x1080 --rate 60 &
