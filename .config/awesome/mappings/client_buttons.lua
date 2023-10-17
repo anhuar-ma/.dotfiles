3242cd8d48917dbcc9766eb78d7a18263072eb8f
anhuar-ma <A01737767@tec.mx>
2023-10-17 12:29:44 -0600

    added awesome

---
+++ .config/awesome/mappings/client_buttons.lua
@@ -0,0 +1,25 @@
+-- Awesome Libs
+local awful = require("awful")
+local gears = require("gears")
+
+local modkey = user_vars.modkey
+
+return gears.table.join(
+  awful.button({}, 1, function(c)
+    c:emit_signal("request::activate", "mouse_click", { raise = true })
+  end),
+  awful.button({ modkey }, 1, function(c)
+    c:emit_signal("request::activate", "mouse_click", { raise = true })
+    awful.mouse.client.move(c)
+  end),
+  awful.button({ modkey }, 3, function(c)
+    c:emit_signal("request::activate", "mouse_click", { raise = true })
+    awful.mouse.client.resize(c)
+  end),
+
+    -- Enable sloppy focus, so that focus follows mouse.
+    client.connect_signal("mouse::enter", function(c)
+        c:activate { context = "mouse_enter", raise = false }
+    end)
+
+)
---
+++ .config/awesome/src/tools/auto_starter.lua
@@ -0,0 +1,8 @@
+local awful = require("awful")
+
+return function(table)
+  for _, t in ipairs(table) do
+    awful.spawn(t);
+  end
+
+end
